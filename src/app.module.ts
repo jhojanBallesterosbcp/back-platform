9511a0540278944d4e8abeefffb312fad6ec0ffa
Jhojan Ballesteros <jhojan@bcpartnerstech.com>
2024-05-17 08:42:07 -0500

    add: ssl

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,6 +26,15 @@ import {ConfigModule} from '@nestjs/config';
 			database: process.env.POSTGRES_DATABASE,
 			autoLoadEntities:true,
 			synchronize:true,
+			ssl: process.env.POSTGRES_SSL === "true",
+      extra: {
+        ssl:
+          process.env.POSTGRES_SSL === "true"
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
 			entities: [User, Company,   
 				Contact
 			],
